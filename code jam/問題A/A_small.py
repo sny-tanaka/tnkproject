1a7569aa5161853f0b381e3f90eeab2b844f1e21
sny-tanaka <s05.t12.030@gmail.com>
2018-02-27 10:13:13 +0900

    update

--- code jam/問題A/A_small.py
+++ code jam/問題A/A_small.py
@@ -1,17 +1,6 @@
 #! python3
 # coding: utf-8
-import os
-
-#読み込むファイルの指定
-inp = os.path.dirname(__file__)+"\A-small-practice.in"
-oup = os.path.dirname(__file__)+"\A-small-output.txt"
-
-#入力ファイルの読み込み
-f = open(inp, 'r')
-numofline = int(f.readline()) #行数
-
-#出力ファイルの読み込み
-g = open(oup, 'w')
+from sys import stdin, stderr
 
 #カットする処理
 def cut(cardlist, n, m):
@@ -19,14 +8,20 @@ def cut(cardlist, n, m):
     clist = cardlist[:n-1] + cardlist[n-1 + m:] #抜き出された部分以外をくっつける
     return cutted + clist #抜き出した部分を上に乗せる
 
-for i in range(numofline):
-    M, C, W = tuple([int(x) for x in f.readline().split()])
-    cardlist = list(range(1, M + 1)) #カードの初期順序
-    for times in range(C):
-        A, B = tuple([int(x) for x in f.readline().split()])
-        cardlist = cut(cardlist, A, B)
-    g.write("Case #" + str(i + 1) + ": " + str(cardlist[W-1]) + "\n")
-    
-f.close()
-g.close()
-print("終了")
+def main():
+    T = int(stdin.readline()[:-1])
+    for i in range(T):
+        M, C, W = [int(x) for x in stdin.readline()[:-1].split()]
+        cardlist = list(range(1, M + 1)) #カードの初期順序
+        for times in range(C):
+            A, B = [int(x) for x in stdin.readline()[:-1].split()]
+            cardlist = cut(cardlist, A, B)
+        print("Case #" + str(i + 1) + ": " + str(cardlist[W-1]))
+    print("終了", file=stderr)
+
+if __name__ == '__main__':
+    main()
+    '''
+    実行時
+    $ python A_small.py < A-small-practice.in > A-small-output.txt
+    '''
--- code jam/問題B/B_small.py
+++ code jam/問題B/B_small.py
@@ -1,38 +1,18 @@
 #! python3
 # coding: utf-8
-import os
-
-#読み込むファイルの指定
-inp = os.path.dirname(__file__)+"\B-small-practice.in"
-oup = os.path.dirname(__file__)+"\B-small-output.txt"
-
-#入力ファイルの読み込み
-f = open(inp, 'r')
-numofline = int(f.readline()) #行数
-
-#出力ファイルの読み込み
-g = open(oup, 'w')
+from sys import stdin, stderr
 
 #その日最も満足度の高いコーヒーを選択する
 def selectcoffee(coffeelist, times):
-    remlist =[] #その日残っているコーヒーのリスト番号を入れるリスト
-    #消費期限がtimes以上のものだけを抽出する
+    i = -1 #初期化
+    #消費期限がtimes以上のもので一番左の項を選択
     for x in range(len(coffeelist)):
         if coffeelist[x][1] >= times:
-            remlist.append(x)
-    #満足度のみのリストを作成
-    slist = []
-    for x in range(len(remlist)):
-        slist.append(coffeelist[remlist[x]][2])
+            i = x
+            break
     #満足度の最大を抽出
-    if slist:
-        mx = max(slist)
-        #最大値の項を検索
-        for x in range(len(coffeelist)):
-            if coffeelist[x][2] == mx:
-                break
-        i = x
-        print(coffeelist)
+    if i != -1:
+        mx = coffeelist[i][2]
         coffeelist[i][0] = coffeelist[i][0] - 1
         if coffeelist[i][0] == 0:
             coffeelist.pop(i) #残数が０になったものはリストから削除
@@ -40,17 +20,24 @@ def selectcoffee(coffeelist, times):
     else:
         return 0
 
-for i in range(numofline):
-    N, K = tuple([int(x) for x in f.readline().split()])
-    coffeelist = list(range(N))
-    maxSatisfy = 0
-    for kinds in range(N): #リスト内に３つセットでリストを入れ子にする
-        coffeelist[kinds] = list([int(x) for x in f.readline().split()])
-    for times in range(K, 0, -1): #最終日から逆算していく
-        coffeelist.sort(key=lambda x: x[0], reverse=True); coffeelist #残数降順でソートしておく
-        maxSatisfy = maxSatisfy + selectcoffee(coffeelist, times)
-    g.write("Case #" + str(i + 1) + ": " + str(maxSatisfy) + "\n")
-    
-f.close()
-g.close()
-print("終了")
+def main():
+    T = int(stdin.readline()[:-1])
+    for i in range(T):
+        N, K = [int(x) for x in stdin.readline()[:-1].split()]
+        coffeelist = []
+        maxSatisfy = 0
+        for kinds in range(N): #リスト内に３つセットでリストを入れ子にする
+            coffeelist.append(list([int(x) for x in stdin.readline().split()]))
+        for times in range(K, 0, -1): #最終日から逆算していく
+            coffeelist.sort(key=lambda x: x[0], reverse=True); coffeelist #残数降順でソート
+            coffeelist.sort(key=lambda x: x[2], reverse=True); coffeelist #満足度降順でソート
+            maxSatisfy = maxSatisfy + selectcoffee(coffeelist, times)
+        print("Case #" + str(i + 1) + ": " + str(maxSatisfy))
+    print("終了", file=stderr)
+
+if __name__ == '__main__':
+    main()
+    '''
+    実行時
+    $ python B_small.py < B-small-practice.in > B-small-output.txt
+    '''
