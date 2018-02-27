7fe970f810620d1fba4eac626f4da19d6e107073
sny-tanaka <s05.t12.030@gmail.com>
2018-02-27 16:46:38 +0900

    repair

--- code jam/2013予選/問題A/A_small.py
+++ code jam/2013予選/問題A/A_small.py
@@ -1,52 +1,31 @@
 #! python3
 # coding: utf-8
 from sys import stdin, stderr
-'''
-n+1本目の円の面積の一般項は
-2r+4n+1
-n+1本目までの円の面積の合計は
-s = ∑[k=0→n](2r+4k+1)=2r(n+1)+(2n+1)(n+1)
-t<sとなる最小のnが答えとなる
-tの範囲も広いので二分探索で絞っていく
-'''
-#黒い円の累計消費インク量
-def sInk(n, r):
-    s =  2 * r * (n + 1) + (2 * n + 1) * (n + 1)
-    return s
+
+#黒い円を書いた後に残るインクの量を求める
+def remInk(r, s, t):
+    r = r + 1 #今回書く黒い円の半径
+    s = r**2 - s #今回書く黒い円の面積
+    t = t - s
+    return t
 
 def main():
     T = int(stdin.readline()[:-1])
     for i in range(T):
         r, t = [int(x) for x in stdin.readline()[:-1].split()]
-        flg = 0
-        mxn = 2**60
-        mnn = 0
-        n = (mxn - mnn) // 2 + mnn
-        s1 = sInk(n, r)
-        s2 = sInk(n+1, r)
-        while flg == 0:
-            if s1 <= t < s2:
-                print("Case #" + str(i + 1) + ": " + str(n))
-                flg = 1
-                break
-            elif t == s2:
-                print("Case #" + str(i + 1) + ": " + str(n - 1))
-                flg = 1
-                break
-            else:
-                if t < s1:
-                    mxn = n
-                elif t > s1:
-                    min = n
-                n = (mxn - mnn) // 2 + mnn
-                s1 = sInk(n, r)
-                s2 = sInk(n+1, r)
-                
+        s = r**2
+        x = 0
+        while t >= 0:
+            t = remInk(r, s, t)
+            x = x + 1
+            r = r + 2
+            s = r**2
+        print("Case #" + str(i + 1) + ": " + str(x - 1))
     print("終了", file=stderr)
 
 if __name__ == '__main__':
     main()
     '''
     実行時
-    $ python A_large.py < A-large-practice.in > A-large-output.txt
+    $ python A_small.py < A-small-practice.in > A-small-output.txt
     '''
